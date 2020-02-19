03bf68cdea1dcb1222526dc446e7350a614d6ec7
sgvvn <tribhuvan.sudha@gmail.com>
2020-02-19 15:29:48 +0530

    Update new

--- cypress/integration/creating_label_spec.js
+++ cypress/integration/creating_label_spec.js
@@ -110,7 +110,8 @@ describe('Codetree : Add Label Functionality Tests', () => {
     cy.get('h4.modal-title').should(($lis) => {
       expect($lis, 'Title of Window').contain('Edit Label')
     })
-    cy.get('input#name').as('nameInputText').clear();
+    cy.get('input#name').as('nameInputText').should('contain.value',random);
+    cy.get('@nameInputText').clear().should('have.value','');
     cy.get('input.button').last().should('have.value', 'Save Label').as('saveLableButton').click();
     cy.get('@nameInputText').next('div').should('contain', 'Please enter a name');
     cy.get('@nameInputText').type('Updated ' + random);
