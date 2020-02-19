7227017540a18b5e0185fa8f0f8921ee2b317a88
sgvvn <tribhuvan.sudha@gmail.com>
2020-02-19 18:46:50 +0530

    Update creating_label_spec.js

--- cypress/integration/creating_label_spec.js
+++ cypress/integration/creating_label_spec.js
@@ -117,8 +117,10 @@ describe('Codetree : Add Label Functionality Tests', () => {
     cy.get('input.button').last().should('have.value', 'Save Label').as('saveLableButton').click();
     cy.get('@nameInputText').next('div').should('contain', 'Please enter a name');
     cy.get('@nameInputText').type('Updated ' + random);
-    cy.get('#color').as('colorInputText').clear();
+    cy.get('input#color').as('colorInputText').clear();
+    cy.get('@colorInputText').should('have.value', '');
     cy.get('@colorInputText').type('#fbca04');
+    cy.get('@colorInputText').should('have.value', '#fbca04');
     cy.get('@saveLableButton').click();
     cy.get('div.flash-tab-container div').first().should('contain', ' Labels updated')
     cy.get('tbody tr td.col-name').should('contain', 'Updated ' + random);
