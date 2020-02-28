20fb1b404aaece7d6309f6d35687203cb1e91357
sgvvn <tribhuvan.sudha@gmail.com>
2020-02-28 16:49:33 +0530

    Rebase

--- cypress/integration/creating_label_spec.js
+++ cypress/integration/creating_label_spec.js
@@ -144,7 +144,6 @@ describe('Codetree : Add Label Functionality Tests', () => {
 
   it('verify deleted lable removed from created issue and change stage done CRLB_012', () => {
     cy.get('div[data-id="backlog"] h3.board-card-title').contains(random).siblings('ul.issue-labels').children().should('have.length', 0)
-    //cy.get('div[data-id="backlog"] ul.issue-labels').children().should('have.length', 0)
     cy.get('div[data-id="backlog"] .board-card-details h3').contains(random).click();
     cy.wait('@editIssue');
     cy.xpath('//span[contains(text(),"Untriaged")]').last().click({ force: true });
@@ -156,7 +155,6 @@ describe('Codetree : Add Label Functionality Tests', () => {
     clickOn('//span[contains(text(),"Epics")]');
     cy.wait('@addEpics');
     cy.get('div[data-id="backlog"] h3.board-card-title').contains(random).siblings('ul.issue-labels').children().should('have.length', 0)
-    //cy.get('div[data-id="backlog"] ul.issue-labels').children().should('have.length', 0)
     cy.get('div[data-id="backlog"] .board-card-details h3').contains(random).click();
     cy.wait('@editIssue');
     cy.xpath('//span[contains(text(),"Untriaged")]').last().click({ force: true });
--- cypress/integration/edit_issue_spec.js
+++ cypress/integration/edit_issue_spec.js
@@ -1,9 +1,10 @@
 /// <reference types="Cypress" />
 import { randomString, clickOnElement, clickOn, setTextOn, sidestep_login, clear } from './util'
 
-describe('Codetree : Edit issue functionality Tests', () => {
+describe('Codetree : Edit Issue Functionality Tests', () => {
     var random = randomString(4);
     var user;
+    
     before(function () {
         cy.fixture('users.json').as('usersData');
         cy.get('@usersData').then((users) => {
@@ -21,7 +22,7 @@ describe('Codetree : Edit issue functionality Tests', () => {
         cy.route('GET', '/projects/*/cards/*?filter={}').as('verifyCreateIssue');
     })
 
-    it('verify fields at edit issue window', () => {
+    it('verify fields at edit issue window EDISSU_001', () => {
         cy.contains('Add Issue').click();
         cy.wait(400);
         cy.get('#title').type("Test Issue " + random);
@@ -30,73 +31,88 @@ describe('Codetree : Edit issue functionality Tests', () => {
         cy.wait('@createIssue');
         cy.get('button.issue-form-command').click();
         cy.wait('@verifyCreateIssue');
-        cy.get('div.flash-tab-container div').last().should('contain', 'Issue created:').and('contain',random)
+        cy.get('div.flash-tab-container div').last().should('contain', 'Issue created:').and('contain', random)
         cy.get('div[data-id="backlog"] h3.board-card-title').first().should('contain', random).click();
-        cy.get('span.issue-form-title').should('contain',random);
-        cy.get('button.issue-form-command .octicon-pencil').should((btn)=>{
-           expect(btn,'Pencil icon for edit ').to.be.visible; 
+        cy.get('span.issue-form-title').should('contain', random);
+        cy.get('button.issue-form-command .octicon-pencil').should((btn) => {
+            expect(btn, 'Pencil icon for edit ').to.be.visible;
         })
-        cy.get('button.button').last().should('contain','Comment').and('be.disabled');
-      })
+        cy.get('button.button').last().should('contain', 'Comment').and('be.disabled');
+    })
 
-    it.skip('verify popup message by put blank issue title EDISSU_004', () => {
+    it('verify popup message by put blank issue title EDISSU_004', () => {
         cy.get('div[data-id="backlog"] h3.board-card-title').contains(random).click();
-        cy.get('span.issue-form-title').should('contain',random);
+        cy.get('span.issue-form-title').should('contain', random);
         cy.get('button.issue-form-command .octicon-pencil').click();
         cy.get('.issue-title-editor #title').as('inputTextTitle');
         cy.get('button[data-behavior="update-title"]').as('saveButton');
-        cy.get('@saveButton').should((btn)=>{
-            expect(btn,'Save button').to.be.enabled; 
-         })
-        cy.get('.issue-title-editor button[data-behavior="toggle-title"]').should((btn)=>{
-            expect(btn,'Cancel Button').to.be.enabled;
-         })
-        cy.get('@inputTextTitle').should('contain.value',random).clear();
-        cy.get('@inputTextTitle').should('contain.value','')
+        cy.get('@saveButton').should((btn) => {
+            expect(btn, 'Save button').to.be.enabled;
+        })
+        cy.get('.issue-title-editor button[data-behavior="toggle-title"]').should((btn) => {
+            expect(btn, 'Cancel Button').to.be.enabled;
+        })
+        cy.get('@inputTextTitle').should('contain.value', random).clear();
+        cy.get('@inputTextTitle').should('contain.value', '')
         cy.get('@saveButton').click();
         cy.get('div.flash-tab-container div').first().should('contain', 'We had a problem saving data for');
     })
 
-    it.skip('verify editing issue functionality for updating title to cancle button EDISSU_003', () => {
+    it('verify editing issue functionality for updating title to cancel button EDISSU_003', () => {
         cy.get('div[data-id="backlog"] h3.board-card-title').contains(random).click();
-        cy.get('span.issue-form-title').should('contain',random);
-        
+        cy.get('span.issue-form-title').should('contain', random);
+
         cy.get('.issue-form-title').then((title) => {
-        cy.get('button.issue-form-command .octicon-pencil').click();
-        cy.get('.issue-title-editor #title').as('inputTextTitle');
-        cy.get('.issue-title-editor button[data-behavior="toggle-title"]').as('cancelButton');
-        cy.get('@inputTextTitle').should('contain.value',random)
-        cy.get('@cancelButton').click();
-        cy.get('.issue-form-title').should('contain',title.text())
+            cy.get('button.issue-form-command .octicon-pencil').click();
+            cy.get('.issue-title-editor #title').as('inputTextTitle');
+            cy.get('.issue-title-editor button[data-behavior="toggle-title"]').as('cancelButton');
+            cy.get('@inputTextTitle').should('contain.value', random)
+            cy.get('@cancelButton').click();
+            cy.get('.issue-form-title').should('contain', title.text())
         })
     })
 
-    it.skip('verify editing issue functionality for updating title to save button EDISSU_002', () => {
+    it('verify editing issue functionality for updating title to save button EDISSU_002', () => {
         cy.get('div[data-id="backlog"] h3.board-card-title').contains(random).click();
-        cy.get('span.issue-form-title').should('contain',random);
+        cy.get('span.issue-form-title').should('contain', random);
         cy.get('button.issue-form-command .octicon-pencil').click();
         cy.get('.issue-title-editor #title').as('inputTextTitle');
         cy.get('button[data-behavior="update-title"]').as('saveButton');
-        cy.get('@inputTextTitle').should('contain.value',random).clear();
-        cy.get('@inputTextTitle').should('contain.value','')
+        cy.get('@inputTextTitle').should('contain.value', random).clear();
+        cy.get('@inputTextTitle').should('contain.value', '')
         cy.get('@inputTextTitle').type("Update Issue " + random);
         cy.get('@saveButton').click();
-        cy.get('.issue-form-title').should('contain', "Update Issue " +random)
+        cy.get('.issue-form-title').should('contain', "Update Issue " + random)
     })
 
-    it('verify editing issue to checking functionality for updating Description', () => {
+    it('verify editing issue functionality for updating description to cancel button EDISSU_005 EDISSU_007', () => {
         cy.get('div[data-id="backlog"] h3.board-card-title').contains(random).click();
-        cy.get('span.issue-form-title').should('contain',random);
-        cy.get('div[data-section="body"]').last().within(()=>{
-            cy.get('div.timeline-node-body p').should('contain','needs 123')
+        cy.get('span.issue-form-title').should('contain', random);
+        cy.get('div[data-section="body"]').last().within(() => {
+            cy.get('div.timeline-node-body p').should('contain', 'needs 123')
+            cy.get('.timeline-node-header .comment-controls').click();
+        })
+        cy.get('button[data-behavior="toggle-composer"]').as('cancelButton').should('be.enabled')
+        cy.get('@cancelButton').click();
+        cy.get('div[data-section="body"]').last().within(() => {
+            cy.get('div.timeline-node-body p').should('contain', 'needs 123')
+        })
+    })
+
+    it('verify editing issue functionality for updating description to update comment button EDISSU_005 EDISSU_015', () => {
+        cy.get('div[data-id="backlog"] h3.board-card-title').contains(random).click();
+        cy.get('span.issue-form-title').should('contain', random);
+        cy.get('div[data-section="body"]').last().within(() => {
+            cy.get('div.timeline-node-body p').should('contain', 'needs 123')
             cy.get('.timeline-node-header .comment-controls').click();
         })
         cy.get('button[data-behavior="update-body"]').as('updateComment').should('be.enabled')
-        cy.get('textarea#body').last().clear().should('have.value','').type('needs 456');
+        cy.get('textarea#body').last().clear().should('have.value', '').type('needs 456');
         cy.get('@updateComment').click();
         cy.wait('@verifyCreateIssue')
-        cy.get('div[data-section="body"]').last().within(()=>{
-            cy.get('div.timeline-node-body p').should('contain','needs 456')
+        cy.get('div[data-section="body"]').last().within(() => {
+            cy.get('div.timeline-node-body p').should('contain', 'needs 456')
         })
+
     })
 })
