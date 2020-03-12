40cdcfbbe834c37d1f2feb7b86d4897ed3ae81dc
sgvvn <tribhuvan.sudha@gmail.com>
2020-03-12 19:33:24 +0530

    Update creating_updating_editing_epic_spec.js

--- cypress/integration/creating_updating_editing_epic_spec.js
+++ cypress/integration/creating_updating_editing_epic_spec.js
@@ -77,7 +77,7 @@ describe('Codetree : Add Epics functionality Tests', () => {
     cy.get('span.issue-form-title').should('contain', random);
     cy.get('div.issue-form-references div #epic_issue_textcomplete').click();
     cy.get('div.issue-form-references div #epic_issue_textcomplete').type(random);
-    cy.wait(1000)
+    //cy.wait(1000)
     cy.get('#edit_modal_epic_autocomplete_container ul li').first().click();
     cy.get('table[class="compact-table epic-issues"] tbody tr').first().find('td span').should('contain', random);
     clickOnElement('button.issue-form-command', 'last');
