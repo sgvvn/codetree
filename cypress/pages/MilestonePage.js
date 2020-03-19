13d77eb6aca45efd56b89b720a59e3bd1149af60
sgvvn <tribhuvan.sudha@gmail.com>
2020-03-19 14:44:16 +0530

    only run mile

--- cypress/pages/MilestonePage.js
+++ cypress/pages/MilestonePage.js
@@ -106,7 +106,7 @@ class MilestonePage {
     cy.route('GET', '/projects/*/views?include_counts=true&scope=milestones&view_type=').as('verifyMilestoneView');
     cy.get('@milestoneWindow').within(() => {
       cy.get('td.col-milestone a').contains(title).parent().siblings('td.col-settings').should('be.visible');
-      cy.get('td.col-milestone a').contains(title).parent().nextAll('td.col-settings a').click();
+      cy.get('td.col-milestone a').contains(title).parent().nextAll('td.col-settings').children('a').click();
       cy.xpath('//a[@aria-expanded="true"]//following::div//a[@data-behavior="delete"]').eq(0).click();    
       cy.wait('@verifyMilestoneView')
     })
