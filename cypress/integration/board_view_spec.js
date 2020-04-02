524a2f999c7b7a0955e0ddb67a144dff670bb0d4
sudhakar <tribhuvan.sudha@gmail.com>
2020-04-02 16:36:30 +0530

    Boardview

--- cypress/integration/board_view_spec.js
+++ cypress/integration/board_view_spec.js
@@ -2,6 +2,7 @@
 import { randomString, clickOn, setTextOn, clickOnElement, clear, sidestep_login } from './util'
 const MilestonePage = new (require('../pages/MilestonePage'))();
 const LabelPage = new (require('../pages/LabelPage'))();
+const EpicPage = new (require('../pages/EpicPage'))();
 
 describe('Codetree : Board View Tests', () => {
     var random;
@@ -29,17 +30,19 @@ describe('Codetree : Board View Tests', () => {
         cy.location('pathname').should('include', 'projects/' + user.projectId + '/board')
         cy.route('GET', '/projects/*/cards/*?filter={}').as('createIssue');
         cy.route('GET','/projects/*/views?include_counts=true&scope=labels&view_type=').as('verifydeletlabel');
-
+        cy.route("GET", '/projects/*/board?type=epic&_pjax=[data-pjax-container]').as('addEpics');
+        cy.route('GET', '/projects/*/cards/*').as('verifyEpic');
     })
     afterEach(function () {
         cy.xpath('//a/span[contains(text(),"Issues")]').click();
         cy.location('pathname').should('include', 'projects/' + user.projectId + '/board')
+        cy.wait(1000)
         cy.get('h3.board-card-title').contains(random).click({ force: true });
         cy.wait(400)
         cy.get('span.issue-form-title').should('contain', random);
         cy.get('button.issue-form-status').should('contain', 'Open').click();
         clickOnElement('button.issue-form-command', 'last');
-        cy.wait('@createIssue');
+        cy.wait('@verifyEpic');
       })
 
     it('verify issues filtered by selected milestone only MIBV_004', () => {
@@ -56,7 +59,7 @@ describe('Codetree : Board View Tests', () => {
         cy.get('.issue-form-milestone-menu .dropdown-menu ul li').contains(random).click();
         cy.get('a.issue-form-milestone-menu-toggle .title').should('contain', random);
         cy.contains('Create Issue').click();
-        cy.wait('@createIssue');
+        cy.wait('@verifyEpic');
         clickOn('button.issue-form-command');
 
         cy.contains('+ Add a filter').click();
@@ -95,12 +98,12 @@ describe('Codetree : Board View Tests', () => {
         cy.get('span.label-menu-label').contains(random).parent().prev('input').click();
     cy.get('ul[class="issue-labels issue-form-labels"] li').should('contain',random);
     cy.contains('Create Issue').click();
-    cy.wait('@createIssue');
+    cy.wait('@verifyEpic');
     clickOn('button.issue-form-command');
     clickOn('//a/span[contains(text(),"Issues")]');
-    cy.wait('@createIssue');
-        cy.contains('+ Add a filter').click();
-        
+    cy.wait('@verifyEpic');
+    cy.wait(400)
+        cy.contains('+ Add a filter').click();   
         cy.get('label[data-filter="labels"]').click()
         cy.get('[data-name="labels"]  .dropdown  .dropdown-menu').within(()=>{
             cy.get('.menu-item-filter .text-field').type(random);
@@ -117,18 +120,43 @@ describe('Codetree : Board View Tests', () => {
     })
 
     it('verify issues filtered by selected epic only MIBV_006', () => {
+    clickOn('//span[contains(text(),"Epics")]');
+    cy.wait('@addEpics');
+        EpicPage.createEpic(random)
+        cy.wait('@saveEpic')
+        cy.xpath('//a/span[contains(text(),"Issues")]').click();
+        cy.route('GET','/projects/*/views?include_counts=true&scope=issues&view_type=boards').as('boardview')
+        cy.wait('@boardview');
+        cy.location('pathname').should('include', 'projects/' + user.projectId + '/board')
+        cy.contains('Add Issue').click();
+        cy.wait(600);
+        cy.get('#title').type("Test Issue " + random);
+        clickOnElement('a.epic-gear-link', 'last');
+        cy.get('div[class="dropdown-menu epic-menu"] input[class="text-field small"]').last().type(random)
+        cy.get('ul[data-custom-sort="epic"] li.checkable-item.nav-focus').first().should('contain', random).click()
+        cy.contains('Create Issue').click();
+        cy.wait('@verifyEpic');
+        cy.get('button.issue-form-command').click(); 
+        cy.xpath('//a/span[contains(text(),"Issues")]').click();
+        cy.wait('@verifyEpic')
+        cy.wait(500)
         cy.contains('+ Add a filter').click();
         cy.get('label[data-filter="epic"]').click()
         cy.get('[data-name="epic"]  .dropdown  .dropdown-menu').within(()=>{
-            cy.get('.menu-item-filter .text-field').type('EPIC Test Data DND');
+            cy.get('.menu-item-filter .text-field').type(random);
             cy.get('ul .nav-focus label input').click();
             cy.get('.filter-button-container .button').click();
         })
-        cy.route('GET','/projects/*/views?epic=EPIC+Test+Data+DND+&include_counts=true&scope=issues&view_type=boards').as('verifyEpicFilter')
-        cy.wait('@verifyEpicFilter')
+        cy.route('GET','/projects/*/views?epic=*&include_counts=true&scope=issues&view_type=boards').as('verifyEpicFilter')
+        cy.wait('@verifyEpic');
+        cy.wait(1000)
         cy.get('.epic-name').each(($el) => {
-              cy.wrap($el).invoke('text').should('equal', 'EPIC Test Data DND ')
+         cy.wrap($el).invoke('text').should('contain', random)
         })
+        clickOn('//span[contains(text(),"Epics")]');
+        cy.wait('@addEpics');
+        EpicPage.closeEpic(random)
+       
     })
 
 })
